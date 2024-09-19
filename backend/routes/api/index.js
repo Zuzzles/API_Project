9053a1cfbfb6319f13fc6d050eacb7f5b61e89c2
Hannah Sheldon <hannahzsheldon@gmail.com>
2024-09-19 14:46:47 -0700

    Add User authentication Express middleware

--- backend/routes/api/index.js
+++ backend/routes/api/index.js
@@ -1,21 +1,13 @@
 // backend/routes/api/index.js
 const router = require('express').Router();
+const { restoreUser } = require("../../utils/auth.js");
+
+// Connect restoreUser middleware to the API router
+router.use(restoreUser);
 
 // test route
 router.post('/test', function(req, res) {
     res.json({ requestBody: req.body });
 });
-
-// Phase 1 fetch request test
-/*
-fetch ('/api/test', {
-    method: "POST",
-    headers: {
-        "Content-Type": "application/json",
-        "XSRF-TOKEN": "f17Wxcpg-bHHLyPy_y0H9riCBDU8IK2Z25b0"
-    },
-    body: JSON.stringify({ hello: 'world' })
-}).then(res => res.json()).then(data => console.log(data));
-*/
 
 module.exports = router;
