dcf289d1a7e6824201073833466f2aa066bb27ba
Hannah Sheldon <hannahzsheldon@gmail.com>
2024-09-20 13:40:23 -0700

    Phase 4 reconfigure done

--- backend/routes/api/session.js
+++ backend/routes/api/session.js
@@ -1,50 +1,48 @@
 // backend/routes/api/session.js
-const {check} = require('express-validator');
-const {handleValidationErrors} = require('../../utils/validation');
 const express = require('express');
 const { Op } = require('sequelize');
 const bcrypt = require('bcryptjs');
 
 const { setTokenCookie, restoreUser } = require('../../utils/auth');
+// const { check } = require('express-validator');
+// const { handleValidationErrors } = require('../../utils/validation');
 const { User } = require('../../db/models');
 
 const router = express.Router();
 
-const validateLogin = [
-    check('credential')
-        .exists({ checkFalsy: true })
-        .notEmpty()
-        .withMessage('Please provide a valid email or username.'),
-    check('password')
-        .exists({ checkFalsy: true })
-        .withMessage('Please provide a password.'),
-    handleValidationErrors,
-];
+// const validateLogin = [
+//     check('credential')
+//         .exists({ checkFalsy: true })
+//         .notEmpty()
+//         .withMessage('Please provide a valid email or username.'),
+//     check('password')
+//         .exists({ checkFalsy: true })
+//         .withMessage('Please provide a password.'),
+//     handleValidationErrors,
+// ];
 
-
-
-
-//Login
-
-router.post("/" async (req, res, next) => {
+// Log in
+router.post("/", async (req, res, next) => {
     const { credential, password } = req.body;  // Destructure credential and password from the request body
 
-    const user = await.unscoped().findOne({ // Query the database for a user with the given credential
-        where:{
+    const user = await User.unscoped().findOne({ // Query the database for a user with the given credential
+        where: {
             [Op.or]: {
                 username: credential,
                 email: credential,
             },
         }
     });
+
     // If no user is found, return an error
-    if(!user || !bcrypt.compareSync( password, user.hashedPassword.toString())) {
+    if (!user || !bcrypt.compareSync( password, user.hashedPassword.toString())) {
         const err = new Error("Login failed");
         err.status = 401;
         err.title = "Login failed";
-        err.errors = { credential: "The provided credentials were invalid."};
+        err.errors = { credential: "The provided credentials were invalid." };
         return next(err);
     }
+
     // If a user is found, call the setTokenCookie function and return the user
     const safeUser = {
         id: user.id,
@@ -52,8 +50,33 @@ router.post("/" async (req, res, next) => {
         username: user.username,
     };
 
-    await setTokenCookie
-    return res.json({ user: safeUser });
+    await setTokenCookie(res, safeUser);
+
+    return res.json({ 
+        user: safeUser 
+    });
 });
 
-module.exports
+// Restore session user
+router.get("/", (req, res) => {
+    const { user } = req;
+    if (user) {
+        const safeUser = {
+          id: user.id,
+          email: user.email,
+          username: user.username,
+        };
+        return res.json({
+          user: safeUser
+        });
+    } else return res.json({ user: null });
+});
+
+// Log out
+router.delete("/", (_req, res) => {
+      res.clearCookie('token');
+      return res.json({ message: 'success' });
+    }
+);
+
+module.exports = router;
--- backend/routes/api/users.js
+++ backend/routes/api/users.js
@@ -1,7 +1,7 @@
 // backend/routes/api/users.js
 const express = require('express');
 const bcrypt = require('bcryptjs');
-const { setTokenCookie } = require('../../utils/auth');
+const { setTokenCookie, requireAuth } = require('../../utils/auth');
 const { User } = require('../../db/models');
 
 const router = express.Router();
@@ -9,26 +9,25 @@ const router = express.Router();
 //Sign Up
 
 router.post("/", async (req, res) => {
-    const { email, password, username } = req.body; // Destructure email, password, and username from the request body
+    const { email, password, username } = req.body;         // Destructure email, password, and username from the request body
+    const hashedPassword = bcrypt.hashSync(password);       // Hash the password
 
-    const hashedPassword = bcrypt.hashSync(password; // Hash the password
-
-    const user = await User.create({ // Create a new user in the database
+    const user = await User.create({                        // Create a new user in the database
         username,
         email,
         hashedPassword,
     });
 
-    const safeUser = { // Create a safe user object
+    const safeUser = {                                      // Create a safe user object
         id: user.id,
         email: user.email,
         username: user.username,
     };
 
-    await setTokenCookie(res, user); // Call the setTokenCookie function
+    await setTokenCookie(res, user);                        // Call the setTokenCookie function
 
-    return res.json({ // Return the user
-        user, safeUser
+    return res.json({                                       // Return the user
+        user: safeUser
     });
 });
 
