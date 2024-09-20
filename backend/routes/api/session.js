09b852d6b56efe153a3750a4be090ed7ec50e54c
Malachi Maynard <malachi@zenkalen.com>
2024-09-19 22:39:35 -0700

    mod session for p1 of phase 5

--- backend/routes/api/session.js
+++ backend/routes/api/session.js
@@ -1,4 +1,6 @@
 // backend/routes/api/session.js
+const {check} = require('express-validator');
+const {handleValidationErrors} = require('../../utils/validation');
 const express = require('express');
 const { Op } = require('sequelize');
 const bcrypt = require('bcryptjs');
@@ -7,6 +9,20 @@ const { setTokenCookie, restoreUser } = require('../../utils/auth');
 const { User } = require('../../db/models');
 
 const router = express.Router();
+
+const validateLogin = [
+    check('credential')
+        .exists({ checkFalsy: true })
+        .notEmpty()
+        .withMessage('Please provide a valid email or username.'),
+    check('password')
+        .exists({ checkFalsy: true })
+        .withMessage('Please provide a password.'),
+    handleValidationErrors,
+];
+
+
+
 
 //Login
 
