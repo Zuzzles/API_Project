e7e7b64fabb519f2c24090b91a21007110747623
Hannah Sheldon <hannahzsheldon@gmail.com>
2024-11-05 16:07:58 -0800

    reset to commit merge 25 setup

--- backend/db/models/review.js
+++ backend/db/models/review.js
@@ -43,9 +43,9 @@ module.exports = (sequelize, DataTypes) => {
   }, {
     sequelize,
     modelName: 'Review',
-    attributes: {
-      exclude: ['UserId', 'SpotId']
-    }
+    // attributes: {
+    //   exclude: ['UserId', 'SpotId']
+    // }
   });
   return Review;
 };
