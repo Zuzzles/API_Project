aa137bc3e8d278279955a2c5b7149c5e4a6531b6
Hannah Sheldon <hannahzsheldon@gmail.com>
2024-11-26 16:14:53 -0800

    first build

--- backend/routes/index.js
+++ backend/routes/index.js
@@ -7,10 +7,10 @@ const apiRouter = require('./api');
 router.use('/api', apiRouter);
 
 // test route with cookies
-router.get('/hello/world', function(req, res) {
-  res.cookie('XSRF-TOKEN', req.csrfToken());
-  res.send('Hello World!');
-});
+// router.get('/hello/world', function(req, res) {
+//   res.cookie('XSRF-TOKEN', req.csrfToken());
+//   res.send('Hello World!');
+// });
 
 // Static routes
 // Serve React build files in production
@@ -39,20 +39,12 @@ if (process.env.NODE_ENV === 'production') {
 // Add a XSRF-TOKEN cookie in development
 if (process.env.NODE_ENV !== 'production') {
   router.get('/api/csrf/restore', (req, res) => {
-    res.cookie('XSRF-TOKEN', req.csrfToken());
-    return res.json({});
-  });
-} else {
-  // Add a XSRF-TOKEN cookie
-  router.get("/api/csrf/restore", (req, res) => {
     const csrfToken = req.csrfToken();
-    res.cookie("XSRF-TOKEN", csrfToken);
+    res.cookie('XSRF-TOKEN', csrfToken);
     res.status(200).json({
       'XSRF-Token': csrfToken
     });
   });
 }
-
-// ...
 
 module.exports = router;
